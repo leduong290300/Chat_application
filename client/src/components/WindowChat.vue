809fc82f9e5895b8c75b0a4b96d58861b0e71955
leduong290300 <leduong290300@gmail.com>
2021-11-19 11:48:43 +0700

    send-message

--- client/src/components/WindowChat.vue
+++ client/src/components/WindowChat.vue
@@ -1,149 +1,83 @@
 <template>
   <div id="wrapper">
-    <b-row id="header">
-      <b-col sm="6" xs="6" class="header-avatar">
-        <b-avatar size="3rem" variant="info"></b-avatar>
-        <span class="name-meta">John</span>
-      </b-col>
-
-      <b-col sm="6" xs="6" class="header-option">
-        <b-icon class="more" icon="three-dots"></b-icon>
-      </b-col>
-    </b-row>
-    <b-row id="message">
-      <b-row class="message-body">
-        <b-col md="1" lg="1" xl="1" class="avatar">
-          <b-avatar size="3rem" variant="info"></b-avatar>
-        </b-col>
-        <b-col md="11" lg="11" xl="11" class="message">
-          <div class="main">
-            <div class="message-text">Hi,Hello Cac Ban</div>
-            <div class="message-time">12:21AM 14/11/2021</div>
-          </div>
-        </b-col>
-      </b-row>
-      <b-row class="message-body">
-        <b-col md="1" lg="1" xl="1" class="avatar">
-          <b-avatar size="3rem" variant="info"></b-avatar>
-        </b-col>
-        <b-col md="11" lg="11" xl="11" class="message">
-          <div class="main">
-            <div class="message-text">Hi,Hello Cac Ban</div>
-            <div class="message-time">12:21AM 14/11/2021</div>
-          </div>
-        </b-col>
-      </b-row>
-      <b-row class="message-body">
-        <b-col md="1" lg="1" xl="1" class="avatar">
-          <b-avatar size="3rem" variant="info"></b-avatar>
-        </b-col>
-        <b-col md="11" lg="11" xl="11" class="message">
-          <div class="main">
-            <div class="message-text">Hi,Hello Cac Ban</div>
-            <div class="message-time">12:21AM 14/11/2021</div>
-          </div>
-        </b-col>
-      </b-row>
-    </b-row>
+    <div v-if="this.currentChatRoom === null">
+      <ChatSingle />
+    </div>
+    <div v-if="this.currentChatRoom !== null">
+      <ChatRoom />
+    </div>
+    <Message />
     <b-row id="reply">
       <b-col sm="1" xs="1" class="reply-enjoy">
         <b-icon icon="emoji-smile" class="icon icon-smile"></b-icon>
       </b-col>
       <b-col sm="9" xs="9" class="reply-main">
-        <textarea rows="1" class="form-control"></textarea>
+        <textarea
+          rows="1"
+          v-model.trim="message"
+          class="form-control"
+        ></textarea>
       </b-col>
       <b-col sm="1" xs="1" class="reply-recording">
         <b-icon icon="mic-fill" class="icon icon-mic"></b-icon>
       </b-col>
       <b-col sm="1" xs="1" class="reply-send">
-        <b-button variant="outline-primary">Gửi</b-button>
+        <b-button v-on:click="sendMessage" variant="outline-primary"
+          >Gửi</b-button
+        >
       </b-col>
     </b-row>
   </div>
 </template>
 <script>
+import { mapGetters } from "vuex";
+import { database, set, ref } from "../../firebase/config";
+import { push, serverTimestamp } from "@firebase/database";
+import ChatSingle from "./ScreenChat/ChatSingle.vue";
+import ChatRoom from "./ScreenChat/ChatRoom.vue";
+import Message from "./Message/Message.vue";
 export default {
   name: "WindowChat",
+  components: { ChatSingle, ChatRoom, Message },
+  data() {
+    return {
+      message: "",
+      storeMessage: [],
+    };
+  },
+  computed: {
+    ...mapGetters(["currentUser", "currentChatRoom"]),
+  },
+  watch: {
+    currentChatRoom() {
+      this.listenersSnap();
+    },
+  },
+
+  methods: {
+    // Gửi tin nhắn
+    sendMessage() {
+      const postRef = ref(database, `message/${this.currentChatRoom.id}`);
+      const newPostRef = push(postRef);
+      if (this.message.length > 0) {
+        set(newPostRef, {
+          content: this.message,
+          time: serverTimestamp(),
+          user: {
+            displayName: this.currentUser.displayName,
+            photoURL: this.currentUser.photoURL,
+            id: this.currentUser.uid,
+          },
+        });
+      }
+      this.message = "";
+    },
+  },
 };
 </script>
 <style scoped>
 #wrapper {
   height: 100vh;
-}
-#header {
-  padding: 10px 16px 10px 15px;
-  margin: 0;
-  height: 80px;
-  width: 100%;
-  background-color: #eee;
-  z-index: 1000;
-  align-items: center;
-  justify-content: space-between;
-}
-#header .header-avatar {
-  padding: 0 !important;
-  margin: 0 !important;
-}
-#header .name-meta {
-  font-weight: 700;
-  font-size: 100%;
-  padding: 5px;
-  padding-bottom: 0;
-  text-align: left;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-  color: #000;
-}
-#header .header-option .more {
-  float: right;
-}
-#message {
-  padding: 0 !important;
-  margin: 0 !important;
-  border: 1px solid #f7f7f7;
-  height: calc(100% - 160px);
-  flex-direction: column;
-  justify-content: flex-end;
-}
-#message .message-body,
-#message .avatar,
-#message .message {
-  padding: 0 !important;
-  margin: 0 !important;
-}
-#message .avatar {
-  width: auto;
-}
-#message .message-body {
-  margin-bottom: 10px !important;
-}
-#message .message {
-  max-width: 60%;
-}
-#message .main {
-  width: auto !important;
-  padding: 4px 10px 7px !important;
-  border-radius: 10px 10px 10px 0;
-  background: #dcf8c6;
-  font-size: 12px;
-  text-shadow: 0 1px 1px rgb(0 0 0 / 20%);
-  word-wrap: break-word;
-  display: inline-block;
-}
-#message .message-text {
-  margin: 0 !important;
-  padding: 5px !important;
-  word-wrap: break-word;
-  font-weight: 200;
-  font-size: 14px;
-  padding-bottom: 0 !important;
-}
-#message .message-time {
-  margin: 0 !important;
-  margin-left: 50px !important;
-  font-size: 12px;
-  text-align: right;
-  color: #9a9a9a;
 }
 #reply {
   height: 80px;
